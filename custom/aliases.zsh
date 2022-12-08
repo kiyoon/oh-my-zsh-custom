3059fe80247d3795eec79e45823dc16eec921287
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-08 01:14:33 +0000

    envpull alias

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -20,7 +20,23 @@ fi
 alias src='omz reload'
 
 alias ca='conda activate'
+alias cc='conda create -n'
 alias ns='nvidia-smi'
 alias rb='gio trash'
 
 alias ghremote="git remote add origin https://github.com/$1"
+
+
+envpull() {
+	git -C ~/bin/vimrc4ubuntu pull
+	~/bin/vimrc4ubuntu/install-linux.sh
+	nvim +qall
+
+	git -C ~/bin/tmux-conf pull
+	~/.tmux/plugins/tpm/scripts/install_plugins.sh
+	~/.tmux/plugins/tpm/scripts/update_plugin.sh all
+
+	git -C ~/bin/oh-my-zsh-custom pull
+	~/bin/oh-my-zsh-custom/apps-local-install.sh
+	omz reload
+}
