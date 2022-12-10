4713509da116428ce682a13338c788a826b292d2
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-10 12:21:51 +0000

    fzf select multiple, select all

--- custom/env.zsh
+++ custom/env.zsh
@@ -8,6 +8,7 @@ export TERMINFO="$HOME/.local/share/terminfo"  # tmux needs this
 # setup fzf Ctrl+t and Alt+c
 if (( $+commands[fzf] )); then
 	if (( $+commands[fd] )); then
+		export FZF_DEFAULT_OPTS='-m --bind ctrl-s:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all'
 		export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
 		export FZF_CTRL_T_COMMAND='fd --type f --hidden --exclude .git'
 		export FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git'
